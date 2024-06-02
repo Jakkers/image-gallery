67b58a2c27340588eb2f895a6cc26892ce9700a2
Jake Darby <designdarby@gmail.com>
2024-06-02 19:49:31 +0100

    Uploading completed assignment with README

--- README.md
+++ README.md
@@ -6,14 +6,30 @@ An assignment to create an image gallery with full accessibility and image optim
 
 (Required)
 🎯 Please mention the requirements you met and which goals you achieved for this assignment.
+• Thumbnails load quickly
+• Larger image displays on selection of thumbnail
+• Accessible conciderations including alternative text for images, and the ability to cycle through images with the arrow keys
+• One page, with a set of thumbnail images, and a larger image
+• Styled appropriately with CSS, and make use of media queries.
+• Use client-side JavaScript to display the larger image when a thumbnail is selected
 
 🎯 Were there any requirements or goals that you were not quite able to achieve?
+• Use client-side JavaScript to navigate between images using the keyboard (tab, enter). This is because it was battling the arrow keys, I wanst sure how to resolve this in time.
+• Voiceover = Not enough time to attempt
+• I managed to acheive a focus ring around objects to highlight what is selected, only problem is this conflicted with the arrow keys as stated above. code is left marked out for review.
 
 🎯 If so, could you please tell us what was it that you found difficult about these tasks?
+• The understanding of how things are conflicting.
 
 (Optional)
 🏹 Feel free to add any other reflections you would like to share about your submission e.g.
+• I have to give thanks to Manny, Joe, Alex, Theo and Richard for taking the time to explain things, collaborate, and share insights.
+• A few of us jumped on the discord channel to discuss over the weekend and that really helped to get past blocks.
 
 What went really well and what could have gone better?
 Detailing useful external sources that helped you complete the assignment (e.g Youtube tutorials).
+• For the key event listener: https://youtu.be/lNJMDTSkNXo?si=60ysbuGCVqCG6n2X
+• Going over previous topics on MDN
+• For optimising images: https://imagecompressor.com/
 Describing errors or bugs you encountered while completing your assignment.
+• See above for conflicting arrows and tab focus. element was undefined.
--- app.js
+++ app.js
@@ -4,55 +4,55 @@ const images = [
     thumbSrc: "mountain-1-250px.jpeg",
     largeSrc: "mountain-1.jpeg",
     alt: "A mountain range at night with a beautiful galaxy in the background",
-    classNameThumb: "thumbnail-image-1",
+    classNameThumb: "thumbnail-image",
   },
   {
     thumbSrc: "mountain-2-250px.jpeg",
     largeSrc: "mountain-2.jpeg",
     alt: "A mountain range with a blanket of clouds in the foreground",
-    classNameThumb: "thumbnail-image-2",
+    classNameThumb: "thumbnail-image",
   },
   {
     thumbSrc: "mountain-3-250px.jpeg",
     largeSrc: "mountain-3.jpeg",
     alt: "A snowy mountain range on a partly cloudy day",
-    classNameThumb: "thumbnail-image-3",
+    classNameThumb: "thumbnail-image",
   },
   {
     thumbSrc: "mountain-4-250px.jpeg",
     largeSrc: "mountain-4.jpeg",
     alt: "A mountain range with a pine forest in the forground",
-    classNameThumb: "thumbnail-image-4",
+    classNameThumb: "thumbnail-image",
   },
   {
     thumbSrc: "mountain-5-250px.jpeg",
     largeSrc: "mountain-5.jpeg",
     alt: "A partly sillouetted mountain range with a clear sky at twilight",
-    classNameThumb: "thumbnail-image-5",
+    classNameThumb: "thumbnail-image",
   },
   {
     thumbSrc: "mountain-6-250px.jpeg",
     largeSrc: "mountain-6.jpeg",
     alt: "A mountain range with a sunset in the background",
-    classNameThumb: "thumbnail-image-6",
+    classNameThumb: "thumbnail-image",
   },
   {
     thumbSrc: "mountain-7-250px.jpeg",
     largeSrc: "mountain-7.jpeg",
     alt: "A mountain valley with the sun shining through",
-    classNameThumb: "thumbnail-image-7",
+    classNameThumb: "thumbnail-image",
   },
   {
     thumbSrc: "mountain-8-250px.jpeg",
     largeSrc: "mountain-8.jpeg",
     alt: "A mountain range in the dark with a purple looking mid mid-ground",
-    classNameThumb: "thumbnail-image-8",
+    classNameThumb: "thumbnail-image",
   },
   {
     thumbSrc: "mountain-9-250px.jpeg",
     largeSrc: "mountain-9.jpeg",
     alt: "A snowy mountain range with a blanket of clouds in the foreground",
-    classNameThumb: "thumbnail-image-9",
+    classNameThumb: "thumbnail-image",
   },
 ];
 
@@ -66,7 +66,9 @@ console.log(largeContainer);
 let displayImage = document.getElementById("large-image-display");
 console.log(displayImage);
 
-images.forEach((image) => {
+let i = 0;
+
+images.forEach((image, index) => {
   // Create a new img element
   let img = document.createElement("img");
 
@@ -74,29 +76,71 @@ images.forEach((image) => {
   img.src = `./assets/${image.thumbSrc}`;
   img.alt = `${image.alt}`;
   img.className = `${image.classNameThumb}`;
+  img.tabIndex = 0;
 
   // Append the img element to the container
   thumbContainer.appendChild(img);
 
-  //event to change image on click
+  //image changes to selected. Also resets index so button order matches.
   img.addEventListener("click", () => {
+    i = index;
     displayImage.src = `./assets/${image.largeSrc}`;
   });
-  //change image on button click
-  const next = document.querySelector(".next");
-  console.log(next);
+});
 
-  next.addEventListener("click", () => {
-    displayImage.src = `./assets/${image.largeSrc}`;
-  });
+tabIndex = 0;
+
+//change image on button click
+const next = document.querySelector(".next");
+console.log(next);
+
+const previous = document.querySelector(".previous");
+console.log(previous);
+
+//creates sequential order and cycles
+console.log(i);
+previous.addEventListener("click", () => {
+  i = (i - 1 + images.length) % images.length;
+  displayImage.src = `./assets/${images[i].largeSrc}`;
+});
+next.addEventListener("click", () => {
+  i = (i + 1) % images.length;
+  displayImage.src = `./assets/${images[i].largeSrc}`;
 });
 
 //allows elements to be focused on
 const elements = document.querySelectorAll("button, .thumbnail-image");
 console.log(elements);
 
-elements.forEach((element) => {
-  element.addEventListener("focus", function (event) {
-    console.log(`Element: ${event.target} is focused`);
-  });
+//! The below code seems to be battling the arrows so i've left off for now
+// element.focus();
+
+// elements.forEach((element) => {
+//   element.addEventListener("focus", function (event) {
+//     console.log(`Element: ${event.target} is focused`);
+//   });
+// });
+
+//arrow keys change image
+document.addEventListener("keydown", (e) => {
+  e.preventDefault();
+  if (e.key === "ArrowRight") {
+    i = (i + 1) % images.length;
+    displayImage.src = `./assets/${images[i].largeSrc}`;
+  }
 });
+
+document.addEventListener("keydown", (e) => {
+  e.preventDefault();
+  if (e.key === "ArrowLeft") {
+    i = (i - 1 + images.length) % images.length;
+    displayImage.src = `./assets/${images[i].largeSrc}`;
+  }
+});
+
+//Enter to select image
+// document.addEventListener("keypress", (event) => {
+//   e.preventDefault();
+//   if (event.key === "Enter") {
+//   }
+// });
